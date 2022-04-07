4f8ee8d9738b0cefaf4580d06d10af23a7f41ab6
cyril <cyril.idu2@gmail.com>
2022-04-07 16:15:52 -0700

    Hello!

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+ *main - begin the function
+ *
+ *Return (0) - end of the function indicating success
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
