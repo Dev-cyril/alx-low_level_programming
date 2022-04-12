bdd6cd43f418b690ceddd7cb0ce2a6172d3af055
cyril <cyril.idu2@gmail.com>
2022-04-12 11:34:54 -0700

    llast digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,10 +11,14 @@ int print_last_digit(int r)
 	int i;
 
 	if (r >= 0)
+	{
 		_putchar((r % 10) + '0');
 		return (r % 10);
+	}
 	else
+	{
 		i = (r * -1) % 10;
 		_putchar(i + '0');
 		return (i);
+	}
 }
