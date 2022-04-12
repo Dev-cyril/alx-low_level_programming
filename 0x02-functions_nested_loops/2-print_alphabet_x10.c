bc8e65be4f9fa7425bb0f265ef57e222838a021b
cyril <cyril.idu2@gmail.com>
2022-04-12 08:40:02 -0700

    10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - check description
+ * Description: prints alphabets in small letter in 10 new lines
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char k;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (k = 'a'; k <= 'z'; k++)
+			_putchar(\n);
+		_putchar('\n');
+	}
+}
