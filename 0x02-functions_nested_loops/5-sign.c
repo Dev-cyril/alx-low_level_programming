0004cf1b7a14febf91a9d1b1a5b4a32eafacaf0b
cyril <cyril.idu2@gmail.com>
2022-04-12 10:23:55 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the signs upon comparision and returns a value
+ * @n: an input number
+ * return: based on the comparitor
+ */
+
+int print_sign(int n)
+{
+	int ret;
+
+	if (n > 0)
+	{
+		ret = 1;
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		ret = 0;
+		_putchar('0');
+	}
+	else
+	{
+		ret = -1;
+		_putchar('-');
+	}
+	return (ret);
+}
