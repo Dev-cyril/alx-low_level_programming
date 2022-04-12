1b76d32d5b78ce582e706a1fcf1a3379143aa232
cyril <cyril.idu2@gmail.com>
2022-04-12 08:04:50 -0700

    put char

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,8 +5,13 @@
  * Return: Always 0
  */
 
-int _putchar(char c)
+int main(void)
 {
-	_putchar('_putchar');
+	char word[8] = "_putchar";
+	int i;
+
+	for (i=0; i < 8; i++)
+		_putchar(word[i]);
+	_putchar('\n');
 	return (0);
 }
