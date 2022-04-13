b10324db937df74c67190c86fa49ee9114bb434d
cyril <cyril.idu2@gmail.com>
2022-04-13 11:21:11 -0700

    fibonacci 4

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	unsigned long n3, n2 = 2, n1 = 1;
+
+	printf("%lu, %lu, ", n1, n2);
+	for (i = 2; i < 98; i++)
+	{
+		n3 = n1 + n2;
+		n1 = n2;
+		n2 = n3;
+		if (i == 97)
+			printf("%lu\n", n3);
+		else
+			printf("%lu, ", n3);
+	}
+	return (0);
+}
