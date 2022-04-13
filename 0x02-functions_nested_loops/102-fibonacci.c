55482fb19e575a13aa42032ddee454dfbcd48191
cyril <cyril.idu2@gmail.com>
2022-04-13 09:54:13 -0700

    fibonaxcci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,14 +7,23 @@
 
 int main(void)
 {
-	int i, n3, n2 = 2, n1 = 1;
-	
-	printf("%d, %d, ", n1, n2);
+	int i;
+	long int n3, n2 = 2, n1 = 1;
+
+	printf("%ld, %ld, ", n1, n2);
 	for (i = 3; i < 50; i++)
 	{
 		n3 = n1 + n2;
-		printf("%d, ", n3);
+		printf("%ld, ", n3);
 		n1 = n2;
 		n2 = n3;
+		if (i == 49)
+		{
+			printf("%ld\n", n3);
+		}
+		else
+		{
+			printf("%ld, ", n3);
+		}
 	} return (0);
 }
