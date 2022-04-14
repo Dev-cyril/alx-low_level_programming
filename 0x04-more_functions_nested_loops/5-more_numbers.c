c8de870964d56f31bf7d32af6cde805d966f15e5
cyril <cyril.idu2@gmail.com>
2022-04-14 13:30:23 -0700

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,11 +9,10 @@ void more_numbers(void)
 {
 	int i, k;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (k = 0; k <= 14; k++)
 		{
-	
 			if (k > 9)
 			{
 				_putchar(k / 10 + '0');
