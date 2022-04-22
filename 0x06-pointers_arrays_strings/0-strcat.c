42d7c6ad72186581ff613b93b8b87cb1e8bd0ae9
cyril <cyril.idu2@gmail.com>
2022-04-22 12:51:08 -0700

    concat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *_strcat - a fumction that concatenates two strings
+ * @dest: a string input
+ * @src: a string input
+ * Return: a aconcatenated string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *str = dest;
+
+	while (*dest)
+		dest++;
+
+	while (*src)
+		*dest++ = src++;
+
+	*dest = '\0';
+
+	return (str);
+}
