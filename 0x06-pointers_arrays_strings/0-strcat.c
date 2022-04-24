b85117ca824478a23c2489a2c8ed7b9b991b7aec
cyril <cyril.idu2@gmail.com>
2022-04-24 10:37:59 -0700

    string concat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,11 +4,24 @@
  * _strcat - a fumction that concatenates two strings
  * @dest: a string input
  * @src: a string input
- * Return: a aconcatenated string
+ * Return: a concatenated string
  */
 
 char *_strcat(char *dest, char *src)
 {
-	strcat(dest, src);
+	int count1 = 0, count2 = 0;
+
+	while (*(dest + count1) != '\0')
+		count1++;
+
+	while (count2 >= 0)
+	{
+		*(dest + count1) = *(src + count2);
+		if (*(src + count2) == '\0')
+			break;
+
+		count1++;
+		count2++;
+	}
 	return (dest);
 }
