72c9161c45e5b694505a8235b8d72b6fbacf1ee0
cyril <cyril.idu2@gmail.com>
2022-04-24 11:08:33 -0700

    string concat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,14 +13,15 @@ char *_strncat(char *dest, char *src, int n)
 	int count1  = 0, count2 = 0;
 
 	while (*(dest + count1) != '\0')
+	{
 		count1++;
+	}
 
 	while (count2 < n)
 	{
 		*(dest + count1) = *(src + count2);
 		if (*(src + count2) == '\0')
 			break;
-
 		count1++;
 		count2++;
 	}
