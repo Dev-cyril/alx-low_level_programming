2e0c6f1a3629c621aeac33c827ce3b0f99a0c2f5
cyril <cyril.idu2@gmail.com>
2022-05-11 16:32:53 -0700

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,16 +9,11 @@
  * Desctiption: a structure with the name, age and owner of a dog.
 **/
 
-typedef struct dog
+struct dog
 {
 	char *name;
 	float *age;
 	char *owner;
-} dog_t;
-void init_dog(struct dog *d, char *name, float *aga, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-char *_strncpy(char *dest, char *src, int n);
-int _strlen(char *s);
+}
+
 #endif
