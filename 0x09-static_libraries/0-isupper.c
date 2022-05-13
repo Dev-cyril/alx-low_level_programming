0b2146944c4ac3aa7d738d0f999896064a786856
cyril <cyril.idu2@gmail.com>
2022-05-13 14:12:28 -0700

    update

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isupper - prints letters in ipper case
+ * @c: integer input
+ * Return: 1 if c is in upper case and 0 if otherwise
+ */
+
+int _isupper(int c)
+{
+	char letter;
+	int check = 0;
+
+	for (letter = 'A'; letter <= 'Z'; letter++)
+	{
+		if (letter == c)
+		{
+			check = 1;
+			break;
+		}
+	}
+	return (check);
+}
